b5b072542dde569fcef949bede7beb3d069d7e2c
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2021-11-11 18:50:22 -0600

    Install user packages

--- install/chroot.sh
+++ install/chroot.sh
@@ -14,13 +14,6 @@ install_linux(){
 		"Running mkinitcpio"
 		"Running grub-install"
 		"Running grub-mkconfig" )
-	#"installing wpa_supplicant"
-	#"installing vim-runtime"
-	#"installing git"
-	#"Running post-transaction hooks"
-	#"Installing for i386-pc platform"
-	#"Generating grub configuration file"
-	#"Found linux image: /boot/vmlinuz-linux" )
 
 	# Initialize progress bar
 	progress_bar " Installing Linux" ${#STAT_ARRAY[@]} "${STAT_ARRAY[@]}" &
@@ -44,19 +37,18 @@ install_linux(){
 
 	# Install Linux
 	sed '/^MODULES=/s|()|(btrfs)|' -i /etc/mkinitcpio.conf
-	#echo "FONT=vector-16" > /etc/vconsole.conf || consolefont
 	#sed '/^HOOKS=/s|filesystems|encrypt filesystems|' -i /etc/mkinitcpio.conf
 	echo -e "\nRunning mkinitcpio"
 	mkinitcpio -p linux
 
 	# Install and configure grub
-	pacman --needed --noconfirm --noprogressbar -S zsh vim git tmux
-	#pacman --needed --noconfirm --noprogressbar -S parallel wget openssh dialog wpa_actiond wpa_supplicant
 	echo -e "\nRunning grub-install"
 	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 	echo -e "\nRunning grub-mkconfig"
+
 	#CR_UUID=$(blkid | grep cryptroot | awk '{ print $2 }' | sed 's|"||g')
 	#sed "/^GRUB_CMDLINE_LINUX_DEFAULT=/s|quiet|quiet cryptdevice=${CR_UUID}:cryptroot root=/dev/mapper/cryptroot|" -i /etc/default/grub
+
 	grub-mkconfig -o /boot/grub/grub.cfg
 
 	wait $BAR_ID
@@ -66,13 +58,13 @@ install_linux(){
 configure_users(){
 
 	STAT_ARRAY=( "Setting root password"
-	"Root password set"
-	"Changing shell for root"
-	"Shell changed"
-	"Adding new user"
-	"Setting user password"
-	"Adding user to sudoers"
-	"New user created" )
+		"Root password set"
+		"Changing shell for root"
+		"Shell changed"
+		"Adding new user"
+		"Setting user password"
+		"Adding user to sudoers"
+		"New user created" )
 
 	# Initialize progress bar
 	progress_bar " Configuring users" ${#STAT_ARRAY[@]} "${STAT_ARRAY[@]}" &
@@ -110,8 +102,8 @@ configure_users(){
 install_x(){
 
 	STAT_ARRAY=("Installing xorg"
-	"Installing bspwm configs"
-	"Generating xinitrc")
+		"Installing bspwm configs"
+		"Generating xinitrc")
 
 	# Initialize progress bar
 	progress_bar " Installing Xorg" ${#STAT_ARRAY[@]} "${STAT_ARRAY[@]}" &
@@ -133,18 +125,6 @@ install_x(){
 	echo "sxhkd &" > /home/$USER/.xinitrc
 	echo "exec bspwm" >> /home/$USER/.xinitrc
 
-	#PACKAGES1="alsa-utils mesa xf86-video-{vesa,intel,fbdev} xf86-input-synaptics"
-	#PACKAGES2="bspwm sxhkd dmenu feh" # conky stow xbindkeys
-	##PACKAGES3="xorg-{server,xinit,xclock,twm,xprop,xlsfonts,xfontsel}"
-	#PACKAGES3="xorg-server xorg-xinit xorg-xclock xorg-twm xorg-xprop xorg-xlsfonts xorg-xfontsel"
-	##GOHUDEPS="xorg-fonts-{encodings,alias} xorg-font-utils fontconfig"
-	#GOHUDEPS="xorg-fonts-encodings xorg-fonts-alias xorg-font-utils fontconfig"
-	#
-	#pacman --needed --noconfirm --noprogressbar -S $PACKAGES1
-	#pacman --needed --noconfirm --noprogressbar -S $PACKAGES2
-	#pacman --needed --noconfirm --noprogressbar -S $PACKAGES3
-	#pacman --needed --noconfirm --noprogressbar -S $GOHUDEPS
-
 	[[ -f /home/$USER/.Xauthority ]] && rm /home/$USER/.Xauthority
 
 	wait $BAR_ID
@@ -152,53 +132,57 @@ install_x(){
 
 build(){
 
-#	"installing cargo" rust is not being installed properly
-	STAT_ARRAY=( "installing nodejs"
-	"installing cmake"
-	"installing virtualbox-host"
-	"loop"
-	"Waiting on user scripts"
-	"We're done" )
+	STAT_ARRAY=( "Running user scripts"
+		"Installing gcc"
+		"Installing rust"
+		"Installing command line tools"
+		"Installing additional tools"
+		"Waiting on user scripts"
+		"We're done" )
 
 	# Initialize progress bar
 	progress_bar " Building extras" ${#STAT_ARRAY[@]} "${STAT_ARRAY[@]}" &
 	BAR_ID=$!
 
+
 	# Fetch scripts to be run by $USER
-	wget https://raw.github.com/MarkEmmons/ArchInstaller/master/install/user_scripts.sh
+	echo "Running user scripts..."
+	wget https://raw.github.com/MarkEmmons/Arch/master/install/user_scripts.sh
 	mv user_scripts.sh /usr/bin/user_scripts
 	chmod a+x /usr/bin/user_scripts
-
-	# Install additional packages
-	DEV_PACKAGES="nodejs npm ctags clang cmake python python2 ruby rust cargo imagemagick"
-	WEBDEV_PACKAGES="python-pip gdb yarn mongodb mongodb-tools"
-	LANG_PACKAGES="btrfs-progs valgrind scrot htop"
-	VM_PACKAGES="docker docker-machine virtualbox virtualbox-host-modules-arch"
-
-	pacman --needed --noconfirm --noprogressbar -S $DEV_PACKAGES
 
 	# Add a wait script and log results separately
 	sudo -u $USER user_scripts > /var/log/install/chroot/user_scripts.log 2>&1 &
 	PID=$!
-	#disown
 
-	pacman --needed --noconfirm --noprogressbar -S $WEBDEV_PACKAGES
-	pacman --needed --noconfirm --noprogressbar -S $LANG_PACKAGES
 
-	# Configure docker, for more info consult the wiki
-	pacman --needed --noconfirm --noprogressbar -S $VM_PACKAGES
-	tee /etc/modules-load.d/loop.conf <<< "loop"
-	gpasswd -a $USER docker
+	# Install C/C++ Tools
+	echo "Installing gcc..."
+	pacman --needed --noconfirm --noprogressbar -S gcc gdb valgrind
+
+
+	# Install Rust Tools
+	echo "Installing rust..."
+	pacman --needed --noconfirm --noprogressbar -S rust
+
+
+	# Install Console Tools
+	echo "Installing command line tools..."
+	pacman --needed --noconfirm --noprogressbar -S zsh vim git tmux
+
+
+	# Install Other Tools
+	echo "Installing additional tools..."
+	pacman --needed --noconfirm --noprogressbar -S stow luakit parallel imagemagick scrot htop
+
+	## Configure docker, for more info consult the wiki
+	#pacman --needed --noconfirm --noprogressbar -S docker docker-machine
+	#tee /etc/modules-load.d/loop.conf <<< "loop"
+	#gpasswd -a $USER docker
 
 	# Wait for user scripts to finish
 	echo "Waiting on user scripts"
 	wait $PID
-
-	# Install gohu and wal
-	#cd /home/$USER/packages/gohufont && pacman --noconfirm --noprogressbar -U *.pkg.tar.xz
-	#cd ../wal-git && pacman --noconfirm --noprogressbar -U *.pkg.tar.xz
-	#cd /home/$USER && sudo -u $USER wal -i wallpapers/ATAT.jpg
-	#cd /
 
 	echo "We're done"
 
@@ -274,25 +258,14 @@ ln -s /usr/share/zoneinfo/US/Central /etc/localtime
 hwclock --systohc --utc
 
 install_linux > /var/log/install/chroot/install_linux.log 3>&2 2>&1
-
-#bash </dev/tty
-
-configure_users > /var/log/install/chroot/configure_users.log 3>&2 2>&1
 install_x > /var/log/install/chroot/install_x.log 3>&2 2>&1
-#build > /var/log/install/chroot/build.log 3>&2 2>&1
+build > /var/log/install/chroot/build.log 3>&2 2>&1
+configure_users > /var/log/install/chroot/configure_users.log 3>&2 2>&1
 
 # Enable dhcpcd on VirtualBox
 lspci | grep -e VGA -e 3D | grep VMware > /dev/null && enable_dhcpcd > /var/log/install/chroot/enable_dhcpcd.log 3>&2 2>&1
 
 get_runtime
-#RUN_TIME=$(get_runtime)
-#export RUN_TIME
-#export USER
-#export HOST
-#SHELL="/bin/zsh"
-#export SHELL
 tput setaf 5 && tput bold && echo "Arch Linux has been installed!" && tput sgr0
-#python archey
 
 rm progress_bar.sh
-#rm archey
