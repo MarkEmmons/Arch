a97390c9c178c0eff3befd3b863e95144d078cac
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2021-12-06 03:22:38 -0600

    Install oh-my-zsh

--- install/user_scripts.sh
+++ install/user_scripts.sh
@@ -3,11 +3,14 @@
 # Get dotfiles
 get_dotfiles(){
 
+	lspci | grep -e VGA -e 3D | grep VMware > /dev/null && curl -LJO https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > /dev/null 3>&2 2>&1
+	sh install.sh
+
 	# Retrieve dotfiles
 	[[ -f .xinitrc ]] && rm -f .xinitrc
 	[[ -f .zshrc ]] && rm -f .zshrc
 	mkdir -p $HOME/workspace
-	git clone https://github.com/MarkEmmons/dotfiles.git
+	git clone https://github.com/MarkEmmons/dotfiles.git $HOME/workspace/dotfiles
 
 	# Add dotfile scripts to path and make executable
 	export PATH=$PATH:$HOME/workspace/dotfiles/bin
